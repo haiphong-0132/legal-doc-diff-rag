afea34c66f83f2fe4d0bee4ebb339b079a33f4bd
Kieu Hong Phong <bangbangonline1@gmail.com>
2026-03-16 11:51:05 +0700

    fix: adapt to hierarchical chunking

--- src/core/embedding/embedding.py
+++ src/core/embedding/embedding.py
@@ -1,24 +1,61 @@
-from typing import List, Callable
+from typing import List, Callable, Optional, Dict, Any
 from pydantic import BaseModel
-from src.schemas import ChunkDocument, EmbeddingRequest, EmbeddingResult
+from src.schemas import ChunkDocument, ChunkDocumentForHierarchical, EmbeddingRequest, EmbeddingResult
+from src.core.embedding import decode_section_id, get_payload_context
 
 EmbeddingFunction = Callable[[List[EmbeddingRequest]], List[EmbeddingResult]]
 
 class EmbeddingPipeline(BaseModel):
     """Kết nối chunking module và embedding module và triển khai embedding module """
-    chunk_documents: List[ChunkDocument]
+    chunk_documents: List[ChunkDocument] | List[ChunkDocumentForHierarchical]
+    
+    fully_payload: Optional[Dict[str, Any]] = None
 
     def _to_embedding_requests(self) -> List[EmbeddingRequest]:
-        return [
-            EmbeddingRequest(
-                chunk_id=chunk.metadata.section_id,
-                text=self._enrich_text(chunk)
-            ) for chunk in self.chunk_documents
-        ]
+        print(len(self.chunk_documents))
+        if isinstance(self.chunk_documents[0], ChunkDocument):
+            return [
+                EmbeddingRequest(
+                    id=chunk.metadata.section_id,
+                    text=f'Nội dung: {self._enrich_text(chunk)}'
+                ) for chunk in self.chunk_documents
+            ]
+        elif isinstance(self.chunk_documents[0], ChunkDocumentForHierarchical):
+            requests = []
+            for chunk in self.chunk_documents:
+                section_id = chunk.metadata.section_id
+                texts = []
+
+                if self.full_payload:
+                    # Duyet cay JSON tao ra context cho chunk hien tai dua vao section_id:
+                    # Text:
+                    # Tiêu đề: .....
+                    # Tiêu đề Đoạn: ........
+                    # Khoản:...
+                    #.............
+                    pass
+
+                texts.append(f'Mã đoạn: {decode_section_id(section_id)}')
+                if chunk.tieu_de:
+                    texts.append(f'Tiêu đề: {chunk.tieu_de}')
+                if chunk.noi_dung:
+                    texts.append(f'Nội dung: {chunk.noi_dung}')
+                if chunk.ref:
+                    texts.append(f'Các viện dẫn: {
+                        ','.join(decode_section_id(ref) for ref in chunk.ref)
+                    }')
+
+                requests.append(
+                    EmbeddingRequest(
+                        chunk_id=section_id,
+                        text='\n'.join(texts)
+                    )
+                )           
+
+            return requests
 
     def _enrich_text(self, chunk: ChunkDocument) -> str:
-        # Giả sử ta thêm section_id vào đầu text để embedding có thể học được mối liên hệ giữa section_id và nội dung
-        return f"{chunk.metadata.section_id}\n\n{chunk.text}"
+        return chunk.text
 
     def run(self, embed_fn: EmbeddingFunction) -> List[EmbeddingResult]:
         requests = self._to_embedding_requests()
--- src/core/vector_store/vectorstore.py
+++ src/core/vector_store/vectorstore.py
@@ -5,40 +5,19 @@ from src.core.vector_store.chroma_store import ChromaStore
 
 class VectorStorePipeline(BaseModel):
     """Pipeline để lưu trữ vector vào ChromaDB"""
-    chunks: List[ChunkDocument]
+    # chunks: List[ChunkDocument]
     embeddings: List[EmbeddingResult]
 
     def _to_upsert_requests(self) -> List[ChromaUpsertRequest]:
-        """Kết nối giữa ChunkDocument và EmbeddingResult để tạo dữ liệu upsert cho ChromaDB"""
-
-        embedding_map = {}
-        for e in self.embeddings:
-            if e.chunk_id in embedding_map:
-             raise ValueError(f"Duplicate chunk_id detected in embeddings: {e.chunk_id}")
-            embedding_map[e.chunk_id] = e
-
-        requests = []
-        missing_chunk_ids = []
-
-        for chunk in self.chunks:
-            cid = chunk.metadata.section_id
-            if cid not in embedding_map:
-                missing_chunk_ids.append(cid)
-                continue
-                
-            requests.append(
-                ChromaUpsertRequest(
-                    chunk_id=cid,
-                    vector=embedding_map[cid].vector,
-                    text=chunk.text,
-                    metadata={"section_id": cid}  # Có thể thêm thông tin khác nếu cần
-                )
-            )
-        
-        if missing_chunk_ids:
-            raise ValueError(f"Missing embeddings for chunk IDs: {missing_chunk_ids}")
-
-        return requests
+        """Kết nối với EmbeddingResult để tạo dữ liệu upsert cho ChromaDB"""
+        return [
+            ChromaUpsertRequest(
+                chunk_id=embedding.chunk_id,
+                text=embedding.text,
+                vector=embedding.vector,
+                metadata={"section_id": embedding.chunk_id} # Có thể thêm thông tin khác nếu cần
+            ) for embedding in self.embeddings
+        ]
     
     def run(self, store: ChromaStore, batch_size: int = None) -> None:
         """Upsert toàn bộ chunks vào ChromaStore"""
