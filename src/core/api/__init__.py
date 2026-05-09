e7589ff1234b20616bb20883dd7cf5eebee92348
Kieu Hong Phong <bangbangonline1@gmail.com>
2026-05-09 23:35:23 +0700

    chore: update .gitignore to exclude Host code directory

--- src/core/api/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Remote API clients for accessing hosted embedding and reranking models."""
-
-from .api_client import APIClient
-from .remote_embedding import RemoteEmbeddingModel
-from .remote_reranker import RemoteReranker
-
-__all__ = ["APIClient", "RemoteEmbeddingModel", "RemoteReranker"]
